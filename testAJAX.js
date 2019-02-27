d29d4c5ebf10df9bf63e7cc31212f056fbce1a98
jasdalucl2018 <43848818+jasdalucl2018@users.noreply.github.com>
2019-02-27 19:31:32 +0000

    19-2-27 @19:30 amedning .js file to get content..
    ..of text box and send to server

--- testAJAX.js
+++ testAJAX.js
@@ -1,16 +1,31 @@
 // define the global variable to process the AJAX request
 var xhr;
+
 function callDivChange() {
-	xhr = new XMLHttpRequest();
-	xhr.open("GET", "test.html", true);
-	xhr.onreadystatechange = processDivChange;
-	xhr.send();
+    xhr = new XMLHttpRequest();
+    // get the content of the text box and send it to the server
+    var filename = document.getElementById("filename").value;
+    xhr.open("GET", filename, true);
+    xhr.onreadystatechange = processDivChange;
+    try {
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    } catch (e) {
+        // this only works in internet explorer
+    }
+    xhr.send();
 }
+
 function processDivChange() {
-	if (xhr.readyState < 4)
-		document.getElementById('div1').innerHTML = "Loading...";
-		else if (xhr.readyState === 4) {
-			if (xhr.status == 200 && xhr.status < 300)
-				document.getElementById('div1').innerHTML = xhr.responseText;
-		}
-	}
+    // while waiting response from the server
+    if (xhr.readyState < 4) {
+        document.getElementById('div1').innerHTML = "Loading...";
+    }
+    // 4 = Response from server has been completely
+    else if (xhr.readyState === 4) {
+        // http status between 200 to 299 are all successful
+        if (xhr.status == 200 && xhr.status < 300) {
+            document.getElementById('div1').innerHTML = xhr.responseText;
+        }
+    }
+}
+
