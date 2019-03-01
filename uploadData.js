55386dde0fd012413bb079891bb5719bd5d185c6
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-01 08:07:08 -0500

    19-3-1 @13:06 ammending code in uploaddata.js to make ajax request to server and to hold response

--- uploadData.js
+++ uploadData.js
@@ -6,7 +6,34 @@ function startDataUpload() {
     var surname = document.getElementById("surname").value;
     var module = document.getElementById("module").value;
     var postString = "name=" + name + "&surname=" + surname + "&module=" + module;
+
+// 19-3-3 @12:37 adding code for pop up alert
+
 alert (postString);
 
 
+}
+
+// 19-3-1 @13;03 adding global variable to hold Ajax request
+
+var client; 
+
+// 190-3-1 @13;04 creating function to issue AJAX request
+
+function processData(postString){
+	client = new XMLHttpRequest();
+	client.open('POST','http://developer.cege.ucl.ac.uk:30296/reflectData',true);
+	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	client.onreadystatechange = dataUploaded;
+	client.send(postString);
+p}
+
+// 19-3-1 @13:04 creating code to wait for response from server and process response once it is received
+
+function dataUploaded(){
+	// function listens for server to say data is readyState
+	if (client.readyState === 4){
+		// change data upload result DIV to show response
+		document.getElementById("dataUploadResult").innerHTML = client.responseText;
+	}
 }
