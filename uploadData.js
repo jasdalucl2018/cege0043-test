187e799aa8c91badac0aaa560cc4307d3d20e124
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-01 07:34:15 -0500

    19-3-1 @12:33 amending fuction startDataUpload.js for server messaging on posting form

--- uploadData.js
+++ uploadData.js
@@ -1,3 +1,12 @@
 function startDataUpload() {
 	alert ("start data upload")
+// 19-3-1 @12;31 amending function to get values from form and post mit to server
+
+	var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var module = document.getElementById("module").value;
+    var postString = "name=" + name + "&surname=" + surname + "&module=" + module;
+alert (postString);
+
+
 }
