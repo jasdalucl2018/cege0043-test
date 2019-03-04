0d76642027c67a84503f3367a6c9dc8d5c179659
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-04 06:20:27 -0500

    19-3-4 amending createForm code to show upload at bottom of form

--- uploadData.js
+++ uploadData.js
@@ -1,9 +1,9 @@
 function startDataUpload() {
-	alert ("start data upload")
+    alert ("start data upload")
     
     // 19-3-1 @12;31 amending function to get values from form and post mit to server
 
-	var name = document.getElementById("name").value;
+    var name = document.getElementById("name").value;
     var surname = document.getElementById("surname").value;
     var module = document.getElementById("module").value;
     var postString = "name=" + name + "&surname=" + surname + "&module=" + module;
@@ -13,19 +13,19 @@ function startDataUpload() {
     // split later on if necessary
     var checkString = "";
     for (var i = 1;i< 5;i++){
-    	if (document.getElementById("check"+i).checked === true) {
-    		checkString = checkString +
-    		document.getElementById("check"+i).value + "||"
-    	}
+        if (document.getElementById("check"+i).checked === true) {
+            checkString = checkString +
+            document.getElementById("check"+i).value + "||"
+        }
     }
     postString = postString + "&modulelist="+checkString;
 
     // get the radio button values
     if (document.getElementById("morning").checked) {
-    	postString=postString+"&lecturetime=morning";
+        postString=postString+"&lecturetime=morning";
     }
     if (document.getElementById("afternoon").checked) {
-    	postString=postString+"&lecturetime=afternoon";
+        postString=postString+"&lecturetime=afternoon";
     }
 
     // get the select box values
@@ -40,9 +40,9 @@ function startDataUpload() {
     postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
 
 
-	// 19-3-3 @12:37 adding code for pop up alert
+    // 19-3-3 @12:37 adding code for pop up alert
 
-	alert (postString);
+    alert (postString);
 
     // 19-3-1 @ 13;17 confirm that function actually runs
 
@@ -58,19 +58,20 @@ var client;
 // 190-3-1 @13;04 creating function to issue AJAX request
 
 function processData(postString){
-	client = new XMLHttpRequest();
-	client.open('POST','http://developer.cege.ucl.ac.uk:30296/reflectData',true);
-	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	client.onreadystatechange = dataUploaded;
-	client.send(postString);
+    client = new XMLHttpRequest();
+    client.open('POST','http://developer.cege.ucl.ac.uk:30296/reflectData',true);
+    client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    client.onreadystatechange = dataUploaded;
+    client.send(postString);
 p}
 
 // 19-3-1 @13:04 creating code to wait for response from server and process response once it is received
 
 function dataUploaded(){
-	// function listens for server to say data is readyState
-	if (client.readyState === 4){
-		// change data upload result DIV to show response
-		document.getElementById("dataUploadResult").innerHTML = client.responseText;
-	}
+    // function listens for server to say data is readyState
+    if (client.readyState === 4){
+        // change data upload result DIV to show response
+        document.getElementById("dataUploadResult").innerHTML = client.responseText;
+    }
 }
+
